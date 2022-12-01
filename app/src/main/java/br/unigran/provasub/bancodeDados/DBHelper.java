ba4528ececd33a5876b7f863dde0e500174db8bb
Gabriel <802.239@alunos.unigran.br>
2022-12-01 19:35:52 -0400

    Criação de Banco de Dados

---
+++ app/src/main/java/br/unigran/provasub/bancodeDados/DBHelper.java
@@ -0,0 +1,30 @@
+package br.unigran.provasub.bancodeDados;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, "ListaAbastecimento", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String querySQL =
+                "create table listaAbastecimento(" +
+                        "quilometragem float," +
+                        "quantidadeAbastecida float," +
+                        "data varchar(10)," +
+                        "valor float)";
+
+        sqLiteDatabase.execSQL(querySQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
